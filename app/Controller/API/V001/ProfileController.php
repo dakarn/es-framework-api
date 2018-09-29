4fc15d7fdb96038787b211e7d79148535363e7a5
myusername <myusername@myemaildomain.com>
2018-09-29 23:49:24 +0400

    API

---
+++ app/Controller/API/V001/ProfileController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 29.09.2018
+ * Time: 22:38
+ */
+
+namespace Controller\API\V001;
+
+use System\Controller\AbstractController;
+
+class ProfileController extends AbstractController
+{
+	public function saveAction()
+	{
+
+	}
+
+	public function infoAction()
+	{
+
+	}
+
+	public function registerAction()
+	{
+
+	}
+}
