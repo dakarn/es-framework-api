11b76f3e8baa74d0791f958fa53fb32342834fb7
myusername <myusername@myemaildomain.com>
2018-10-01 01:52:54 +0400

    API

---
+++ bootstrap-api.php
@@ -0,0 +1,35 @@
+<?php
+
+define('PSR_4', true);
+define('IS_DEV', \is_file('dev.php'));
+
+include_once __DIR__ . '/vendor/autoload.php';
+
+$event = new App\AppEvent();
+$event = $event->installEvents(new \System\EventListener\EventManager());
+
+$appKernel = new \App\AppKernel();
+$appKernel->installMiddlewares()->installProviders();
+
+$env = 'PROD';
+
+if (IS_DEV) {
+	$env = 'DEV';
+}
+
+$application = (new \System\Kernel\TypesApp\WebApp())
+	->setEnvironment($env)
+	->setAppEvent($event)
+	->setAppKernel($appKernel)
+	->setApplicationType('Web');
+
+set_exception_handler(function($e) use($application) {
+	$application->outputException($e);
+});
+
+register_shutdown_function(function() use($application) {
+	System\Kernel\ShutdownScript::run();
+});
+
+$application->run();
+$application->outputResponse();
