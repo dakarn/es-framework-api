d711b2bae02a2e64139434d087466e1a90f6f052
myusername <myusername@myemaildomain.com>
2018-10-01 19:00:38 +0400

    API

---
+++ app/ApiApp.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 17.04.2018
+ * Time: 21:55
+ */
+
+namespace App;
+
+use System\Router\Router;
+use System\Kernel\TypesApp\AbstractApplication;
+
+class ApiApp extends AbstractApplication
+{
+	public function run()
+	{
+
+	}
+
+	public function terminate()
+	{
+
+	}
+}
